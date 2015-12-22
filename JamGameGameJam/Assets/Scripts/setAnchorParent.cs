196a35c8534cf6dcc709ad0d5ed22ea41fdcbadc
Lucas Rumney <lucasrumney94@gmail.com>
2015-12-22 17:26:50 -0500

    Added Test Anchor and seeking code for the attractor.

---
+++ JamGameGameJam/Assets/Scripts/setAnchorParent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setAnchorParent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.transform.parent = GameObject.FindGameObjectWithTag("Anchor").transform;
+	}
+
+}
