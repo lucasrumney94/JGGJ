3509c72684832a0de3ecdc8c8a813ec54a592c15
Lucas Rumney <lucasrumney94@gmail.com>
2015-12-22 17:52:11 -0500

    Added basic character movement

--- JamGameGameJam/Assets/playerMovement.cs
+++ JamGameGameJam/Assets/playerMovement.cs
@@ -3,14 +3,30 @@ using System.Collections;
 
 public class playerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
+    public float speed = 5.0f;
+    Rigidbody rb = new Rigidbody();
+    
+    
+    // Use this for initialization
+    void Start ()
     {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(speed*movement*Time.smoothDeltaTime);
+
+    }
+
+
+
+// Update is called once per frame
+void Update ()
     {
 	
 	}
