2bc564adddbdc472aaff0d880d723f9b39e0e37e
Lucas Rumney <lucasrumney94@gmail.com>
2015-12-22 19:08:34 -0500

    Added basic camera controls, Added edible objects, Added elevation control.

---
+++ JamGameGameJam/Assets/rotateCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateCamera : MonoBehaviour {
+
+
+    public float turnSpeed = 4.0f;
+
+    private Vector3 input;
+
+    // Use this for initialization
+    void Start ()
+    {
+       
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        
+        //Debug.Log(inputRotation);
+
+    }
+    void LateUpdate()
+    {
+        float xAxis = Input.GetAxis("rotateCamera");
+        transform.Rotate(new Vector3(0.0f, xAxis*turnSpeed, 0.0f));
+        //float yAxis = Input.GetAxis("rotateCameraVertical");
+       // transform.Rotate(new Vector3(yAxis * turnSpeed, 0.0f, 0.0f));
+    }
+}
