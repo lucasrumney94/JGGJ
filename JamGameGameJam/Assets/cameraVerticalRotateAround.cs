cdfb9057745f29746133748e393520e95659b970
Lucas Rumney <lucasrumney94@gmail.com>
2015-12-23 23:36:01 -0500

    Reverted camera

--- JamGameGameJam/Assets/cameraVerticalRotateAround.cs
+++ JamGameGameJam/Assets/cameraVerticalRotateAround.cs
@@ -11,8 +11,8 @@ public class cameraVerticalRotateAround : MonoBehaviour {
     // Update is called once per frame
     void LateUpdate()
     {
-        //float xAxis = Input.GetAxis("rotateCamera");
-       // transform.parent.transform.parent.transform.Rotate(new Vector3(0.0f, xAxis * turnSpeed, 0.0f)); //rotate the actual Anchor
+        float xAxis = Input.GetAxis("rotateCamera");
+        transform.parent.transform.Rotate(new Vector3(0.0f, xAxis * turnSpeed, 0.0f)); //rotate the actual Anchor
 
 
         //transform.RotateAround(transform.parent.transform.position,new Vector3(0.0f, 1.0f, 0.0f), xAxis * turnSpeed);
