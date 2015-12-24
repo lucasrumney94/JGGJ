1b6d4437f18e192e60448f1b48b928fc417507dd
Hardy Whitney <htw6174@gmail.com>
2015-12-23 22:00:41 -0500

    Blinky AI

---
+++ JamGameGameJam/Assets/Scripts/AI/AI_Blinky.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AI_Blinky : MonoBehaviour {
+
+    public float speed = 1f;
+
+    private GameObject player;
+    private Rigidbody physicsRigidbody;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        physicsRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 toPlayer = this.transform.position - player.transform.position;
+        toPlayer = toPlayer.normalized * speed;
+        physicsRigidbody.AddForce(toPlayer);
+        Debug.DrawRay(transform.position, toPlayer, Color.red);
+    }
+}
