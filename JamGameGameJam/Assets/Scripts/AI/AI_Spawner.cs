73d1f4db57a77dc84fedde3453f3e6fd1d0b942e
Hardy Whitney <htw6174@gmail.com>
2015-12-24 19:46:32 -0500

    New ship model, test spawner in AI scene

---
+++ JamGameGameJam/Assets/Scripts/AI/AI_Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_Spawner : MonoBehaviour {
+
+    public float intervalTime;
+
+    public GameObject spawnable;
+
+    private float startTime;
+    private float lastSpawnTime;
+
+    void Start()
+    {
+        startTime = Time.time;
+        lastSpawnTime = Time.time;
+        Spawn();
+    }
+
+    void Update()
+    {
+        if (Time.time - lastSpawnTime > intervalTime)
+        {
+            Spawn();
+            lastSpawnTime = Time.time;
+        }
+    }
+
+    private void Spawn()
+    {
+        Instantiate(spawnable, transform.position, transform.rotation);
+    }
+}
