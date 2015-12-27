48d166d384f2c281d6d619c488df7d6fe99db0da
Lucas Rumney <lucasrumney94@gmail.com>
2015-12-26 22:54:35 -0500

    Updated HUD to include Score

---
+++ JamGameGameJam/Assets/Scripts/UI/displayPlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class displayPlayerStats : MonoBehaviour {
+
+    private playerStats pStats;
+    private Text myText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        myText = GetComponent<Text>();
+        pStats = GameObject.FindGameObjectWithTag("Player").GetComponent<playerStats>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        myText.text = "Globs: " + pStats.playerGlobCount.ToString() + "!";
+	}
+}
